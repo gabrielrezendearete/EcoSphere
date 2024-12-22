4e3eec0df31ecc850c032b85b947eda3f518b750
Gabriel Rezende <gabrielmacedorezende@gmail.com>
2024-12-22 10:08:39 -0300

    Inclusão de animações e personalização do formulário

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
+/* Animações */
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+const elements = document.querySelectorAll('.fade-in-element');
+elements.forEach(el => observer.observe(el));
+
 /* Botões */
 document.querySelectorAll(".botao-contato").forEach(botao => {
   botao.addEventListener("click", function () {
     document.getElementById("contato").scrollIntoView({ behavior: "smooth" });
   });
 });
+
+/* Mensagem de Confirmação */
+const form = document.getElementById('form');
+    const confirmationMessage = document.getElementById('confirmacao');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+      confirmationMessage.style.display = 'block';
+      form.reset();
+      setTimeout(() => {
+        confirmationMessage.style.display = 'none';
+      }, 5000);
+    });
